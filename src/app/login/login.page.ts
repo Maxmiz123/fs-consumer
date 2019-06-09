d31a04ddb0e29ea5e587b607fb391ee54077fd3f
Max Misrahi <maxmisrahi@Maxs-MacBook-Air.local>
2019-06-09 23:40:04 +0100

    Newest Changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,28 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+
+  public email: string = "";
+  public password: string = "";
 
   constructor(
+    private httpClient: HttpClient,
     private navCtrl: NavController
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  login() {
+    const authReq = {
+      email: this.email,
+      password: this.password
+    };
+    this.httpClient
+    .post("http://localhost:3000/api/auth", authReq)
+    .subscribe(
+      (response: any) => {
+        console.log(response);
+        const userId = response.id;
 
-  navToProfile() {
-    this.navCtrl.navigateForward('tabs');
-  }
-  navToSignup() {
-    this.navCtrl.navigateForward('signup');
+        localStorage.setItem("user_id", userId);
+
+        this.navCtrl.navigateForward("tabs", {
+          queryParams: {
+            user_id: userId
+          }
+        });
+      },
+      err => {
+        alert("Failed to login. Incorrect Username or Password");
+      }
+    );
   }
   navToRegister(){
     this.navCtrl.navigateForward('signup');
-  }
-
+}
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { Property } from '../models/index';
+import { PropertyService } from '../services/property.service';
 
 @Component({
   selector: 'app-tab2',
@@ -8,9 +10,29 @@ import { NavController } from '@ionic/angular';
 })
 export class Tab2Page {
 
+  properties: Array<Property>;
+
+
   constructor(
-    private navCtrl: NavController
-  ) {}
+    private navCtrl: NavController,
+    private propertyService: PropertyService
+
+  ) {
+    this.properties = this.propertyService.getAllProperties();
+
+  }
+
+  navToProperty(property: Property) {
+
+    this.navCtrl
+      .navigateForward("property-details", {
+        queryParams: {
+          q: "ionic",
+          propertyName: property.name,
+          propertyId: property.id
+        }
+      });
+  }
 
   navToProfile() {
     this.navCtrl.navigateForward("tabs");
